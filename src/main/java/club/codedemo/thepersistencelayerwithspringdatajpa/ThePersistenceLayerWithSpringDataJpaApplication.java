108d6e827f15820f5a593076fb9cdac4f1167fd6
panjie <3792535@qq.com>
2020-08-08 14:51:35 +0800

    init

---
+++ src/main/java/club/codedemo/thepersistencelayerwithspringdatajpa/ThePersistenceLayerWithSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package club.codedemo.thepersistencelayerwithspringdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThePersistenceLayerWithSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThePersistenceLayerWithSpringDataJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/club/codedemo/thepersistencelayerwithspringdatajpa/ThePersistenceLayerWithSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package club.codedemo.thepersistencelayerwithspringdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThePersistenceLayerWithSpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
